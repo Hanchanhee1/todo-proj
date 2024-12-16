290e1c315d392a35c43db266b0ad91efda181034
chanhee han <mh981219@naver.com>
2024-12-16 14:19:14 +0900

    11 타입스크립트 강의 - Add Functions 1-1 / 12 타입스크립트 강의 - Add Functions 1-2

--- src/view/TodoConsole.ts
+++ src/view/TodoConsole.ts
@@ -7,7 +7,10 @@ import { Commands } from '../model/Commands';
 class TodoConsole {
     private todoCollection : TodoCollection;
 
+    private showCompleted : boolean;
+
     constructor() {
+        this.showCompleted = true;
         const sampleTodos : TodoItem[] = data.map(
             (item) => new TodoItem(item.id, item.task, item.complete)
         );
@@ -19,7 +22,7 @@ class TodoConsole {
             `=====${this.todoCollection.userName}=====` +
             `(${this.todoCollection.getItemCounts().incomplete} items todo)`
         )
-        this.todoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+        this.todoCollection.getTodoItems(this.showCompleted).forEach((item) => item.printDetails());
     }
 
     promptUser() : void {
@@ -27,15 +30,69 @@ class TodoConsole {
         this.diplayTodoList();
 
         inquirer.prompt({
-            type:'list',
-            name:'command',
-            message:'Choose option',
+            type:"list",
+            name:"command",
+            message:"Choose option",
             choices: Object.values(Commands)
         }).then((answers) => {
-            if(answers ['command'] !== Commands.Quit) {
+           switch(answers["command"]) {
+            case Commands.Toggle:
+                this.showCompleted = !this.showCompleted;
                 this.promptUser();
-            }
+                break;
+            case Commands.Add:
+                this.promptAdd();
+                break;
+            case Commands.Purge:
+                this.todoCollection.removeComplete();
+                this.promptUser();
+                break;
+            case Commands.Complete:
+                if(this.todoCollection.getItemCounts().incomplete > 0) {
+                    this.promptComplete();
+                }else {
+                    this.promptUser();
+                }
+                break;
+           }
         });
+    }
+
+    promptAdd() : void {
+        console.clear();
+        inquirer.prompt({
+            type:"input",
+            name:"add",
+            message:"Enter task :"
+        }).then((answers) => {
+            if(answers["add"] !== "") {
+                this.todoCollection.addTodo(answers["add"]);
+            }
+            this.promptUser();
+        })
+    }
+
+    promptComplete(): void {
+        console.clear();
+        inquirer.prompt({
+            type:"checkbox",
+            name:"complete",
+            message:"Mark Tasks Complete",
+            choices:this.todoCollection.getTodoItems(this.showCompleted).map((item) => ({
+                name:item.task,
+                value:item.id,
+                checked:item.complete
+            }))
+        }).then((answers) => {
+            let completedTasks = answers["complete"] as number[];
+            this.todoCollection.getTodoItems(true).forEach((item) => 
+                this.todoCollection.markComplete(
+                    item.id,
+                    completedTasks.find((id) => id === item.id) != undefined
+                )
+            )
+            this.promptUser();
+        })
     }
 
 }
